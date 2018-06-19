820b000508cb4f04183be8ce9ba2e26307603d49
NavonilDas <navneeladas@gmail.com>
2018-06-20 01:16:47 +0530

    Fixed Some bugs

---
+++ insertdata.sql
@@ -0,0 +1,38 @@
+CREATE TABLE clg_names{
+    name varchar(200) UNIQUE,
+    city char(50),
+    state char(50),
+    country char(50)
+}
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM scilab.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM esim.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM dwsim_2015.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM cfd_2015.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM openmodelica.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM r_2017.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;    
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM scilab.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM esim.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM cfd_2015.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM dwsim_2015.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM openmodelica.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM or_fossee.textbook_companion_proposal l WHERE l.university NOT IN (	SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM r_2017.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.venue,l.city,l.state,l.country FROM fossee_new.workshop l WHERE l.venue NOT IN (	SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.venue;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM esim.esim_circuit_simulation_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
+
+INSERT INTO fossee_new.clg_names SELECT l.university,l.city,l.state,l.country FROM dwsim_2015.dwsim_flowsheet_proposal l WHERE l.university NOT IN (SELECT c.name FROM fossee_new.clg_names c) GROUP BY l.university;
