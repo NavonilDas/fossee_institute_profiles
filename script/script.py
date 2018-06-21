867b63cec82367ad0974d1e65cab5fcafc6ee738
NavonilDas <navneeladas@gmail.com>
2018-06-21 12:50:47 +0530

    Improved the scrpt

--- script/script.py
+++ script/script.py
@@ -1,8 +1,13 @@
 import pymysql
 import re
 
-file = open("del.sql",'w') #open the sql file
+# Required Variables
+db_name = 'fossee_new' # Default database
+table_name = 'clg_names'
 
+file = open("del.sql",'w') #open the sql file
+file.write('use '+db_name)
+file.write('\n')
 #words to be removed
 nre = ['.',' engineering',' group',' college',
 ' technology',' institution','dr',
@@ -12,7 +17,7 @@ nre = ['.',' engineering',' group',' college',
 tospace = [',']
 
 conn = pymysql.connect(
-    host="localhost",user="root",password="",db="fossee_new"
+    host="localhost",user="root",password="",db=db_name
 )
 a = conn.cursor()
 query = "SELECT * FROM clg_names ORDER BY CHAR_LENGTH(name)"
@@ -43,13 +48,13 @@ for i in range(0,l-1):
         if(match >= 1):
             if(len(clgdic[j]['orig']) > len(clgdic[i]['orig'])):
                 if(clgdic[i]['orig'] not in selected):
-                    que = 'DELETE FROM clg_names WHERE name = \"'+clgdic[i]['orig']+'\";'
+                    que = 'DELETE FROM '+table_name+' WHERE name = \"'+clgdic[i]['orig']+'\";'
                     selected.append(clgdic[i]['orig'])
                     file.write(que)
                     file.write('\n')
             else:
                 if(clgdic[j]['orig'] not in selected):
-                    que = 'DELETE FROM clg_names WHERE name = \"'+clgdic[j]['orig']+'\";'
+                    que = 'DELETE FROM '+table_name+' WHERE name = \"'+clgdic[j]['orig']+'\";'
                     selected.append(clgdic[j]['orig'])
                     file.write(que)
                     file.write('\n')
