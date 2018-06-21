cd8ff171a9748ba1d5160c594908258cc1e9195e
NavonilDas <navneeladas@gmail.com>
2018-06-21 12:33:27 +0530

    Added Script to Remove Same names from table

---
+++ script/script.py
@@ -0,0 +1,57 @@
+import pymysql
+import re
+
+file = open("del.sql",'w') #open the sql file
+
+#words to be removed
+nre = ['.',' engineering',' group',' college',
+' technology',' institution','dr',
+' university',' institute',' eng',' management',' national',' chemical',
+' science',' of',' tech',' and',' &']
+
+tospace = [',']
+
+conn = pymysql.connect(
+    host="localhost",user="root",password="",db="fossee_new"
+)
+a = conn.cursor()
+query = "SELECT * FROM clg_names ORDER BY CHAR_LENGTH(name)"
+a.execute(query)
+clgdic = []
+tmp = {}
+datas = a.fetchall() # fetch all the rows
+selected = [] #temp dictonary to store Alerady deleted values
+
+for data in datas:
+    tmp = {}
+    x = data[0].lower()
+    for i in range(0,18):
+        x = x.replace(str(nre[i]),'')
+    tmp['name'] = x.replace(',',' , ').replace('-',' ').split()
+    tmp['city'] = data[1]
+    tmp['state'] = data[2]
+    tmp['orig'] = data[0]
+    if(len(data[0].replace(" ","")) > 5):
+        clgdic.append(tmp)
+
+l = len(clgdic)
+for i in range(0,l-1):
+    for j in range(i+1,l):
+        match = 0
+        for val in clgdic[i]['name']:
+            if((val.replace(" ","") in " ".join(clgdic[j]['name'])) and val != ','): match += 1
+        if(match >= 1):
+            if(len(clgdic[j]['orig']) > len(clgdic[i]['orig'])):
+                if(clgdic[i]['orig'] not in selected):
+                    que = 'DELETE FROM clg_names WHERE name = \"'+clgdic[i]['orig']+'\";'
+                    selected.append(clgdic[i]['orig'])
+                    file.write(que)
+                    file.write('\n')
+            else:
+                if(clgdic[j]['orig'] not in selected):
+                    que = 'DELETE FROM clg_names WHERE name = \"'+clgdic[j]['orig']+'\";'
+                    selected.append(clgdic[j]['orig'])
+                    file.write(que)
+                    file.write('\n')
+print('filesaved')
+conn.close()
