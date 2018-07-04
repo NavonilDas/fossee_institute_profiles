af07b771dc573660da72af35e080fb0d407581aa
NavonilDas <navneeladas@gmail.com>
2018-07-04 22:07:36 +0530

    Removed screenshots

---
+++ insertdata.sql
@@ -0,0 +1,16 @@
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM scilab.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM esim.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM dwsim_2015.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM cfd_2015.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM openmodelica.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM r_2017.lab_migration_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM scilab.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM esim.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM cfd_2015.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM dwsim_2015.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM openmodelica.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM or_fossee.textbook_companion_proposal l WHERE l.university NOT IN (	SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM r_2017.textbook_companion_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.venue,l.city,l.state,l.country FROM drupal8.workshop l WHERE l.venue NOT IN (	SELECT c.name FROM drupal8.clg_names c) GROUP BY l.venue;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM esim.esim_circuit_simulation_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
+INSERT INTO drupal8.clg_names SELECT l.university,l.city,l.state,l.country FROM dwsim_2015.dwsim_flowsheet_proposal l WHERE l.university NOT IN (SELECT c.name FROM drupal8.clg_names c) GROUP BY l.university;
